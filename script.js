495fc6c22bc81070ac38d9002c84c16de80580d7
MohithAYANURU <mohith.ayanuru@gmail.com>
2025-09-19 21:51:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector(".header__toggle-btn")
+
+const header = document.querySelector(".header")
+
+toggleBtn.addEventListener("click", () => {
+
+  header.classList.toggle("header-open")
+
+  console.log("Header toggled!")
+})
